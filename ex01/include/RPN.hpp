6e12bc1e3411e7f348acf8620da610e201221f83
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-08-06 10:32:18 +0900

    [ex01] Formatter

--- ex01/include/RPN.hpp
+++ ex01/include/RPN.hpp
@@ -1,27 +1,26 @@
 #ifndef RPN_HPP
-# define RPN_HPP
+#define RPN_HPP
 
-# include <stack>
-# include <sstream>
-# include <iostream>
+#include <iostream>
+#include <sstream>
+#include <stack>
 
-class RPN
-{
-private:
+class RPN {
+  private:
 	static std::stack<std::string> stack_;
 	RPN();
-	RPN(const RPN& other);
-	RPN& operator=(const RPN& other);
+	RPN(const RPN &other);
+	RPN &operator=(const RPN &other);
 	~RPN();
 
-public:
+  public:
 	static int add(int a, int b);
 	static int subtract(int a, int b);
 	static int multiply(int a, int b);
-	static int devide(int a, int b);
+	static int divide(int a, int b);
 
 	static void pushNumbers(std::string line);
-	static void calculate(const std::string& op);
+	static void calculate(const std::string &op);
 
 	static const std::string OP;
 };
--- ex01/src/RPN.cpp
+++ ex01/src/RPN.cpp
@@ -1,70 +1,57 @@
 #include "RPN.hpp"
 
 std::stack<std::string> RPN::stack_;
-const std::string RPN::OP = "+-/*";
+const std::string       RPN::OP = "+-/*";
 
-void RPN::pushNumbers(std::string line)
-{
+void RPN::pushNumbers(std::string line) {
 	std::stringstream iss(line);
-	std::string token;
-	while (iss >> token)
-	{
-		if (token.length() >= 2)
-		{
+	std::string       token;
+	while (iss >> token) {
+		if (token.length() >= 2) {
 			std::cout << "Error" << std::endl;
 			std::exit(EXIT_FAILURE);
-		}
-		else if (std::isdigit(token[0]))
+		} else if (std::isdigit(token[0]))
 			stack_.push(token);
 		else if (std::strchr(RPN::OP.c_str(), token[0]) && stack_.size() >= 2)
 			calculate(token);
 	}
 	if (stack_.size() == 1)
 		std::cout << stack_.top() << std::endl;
-	else
-	{
+	else {
 		std::cout << "Error" << std::endl;
 		std::exit(EXIT_FAILURE);
 	}
 }
 
-int RPN::add(int a, int b)
-{
+int RPN::add(int a, int b) {
 	return a + b;
 }
 
-int RPN::subtract(int a, int b)
-{
+int RPN::subtract(int a, int b) {
 	return a - b;
 }
 
-int RPN::multiply(int a, int b)
-{
+int RPN::multiply(int a, int b) {
 	return a * b;
 }
 
-int RPN::devide(int a, int b)
-{
-	if (b == 0)
-	{
+int RPN::divide(int a, int b) {
+	if (b == 0) {
 		std::cout << "Error" << std::endl;
 		std::exit(EXIT_FAILURE);
 	}
 	return a / b;
 }
 
-void RPN::calculate(const std::string& op)
-{
+void RPN::calculate(const std::string &op) {
 	int b = std::atoi(stack_.top().c_str());
 	stack_.pop();
 	int a = std::atoi(stack_.top().c_str());
 	stack_.pop();
 	typedef int (*FuncPtr)(int, int);
-	FuncPtr funcs[] = {&RPN::add, &RPN::subtract, &RPN::devide, &RPN::multiply};
-	for (int i = 0; i < 4; i++)
-	{
-		if (op[0] == RPN::OP[i])
-		{
+	FuncPtr funcs[] = {&RPN::add, &RPN::subtract, &RPN::divide, &RPN::multiply};
+	for (int i = 0; i < 4; i++) {
+		if (op[0] == RPN::OP[i]) {
 			std::stringstream ss;
 			ss << (funcs[i])(a, b);
 			stack_.push(ss.str());
