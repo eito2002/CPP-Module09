af86b0456e419379708e8d5724edd8feb14ceceb
eito2002 <126877725+eito2002@users.noreply.github.com>
2024-10-20 00:23:11 +0900

    [ex02] main関数を関数分け

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,6 +6,26 @@ namespace {
 typedef std::vector<int> IntVector;
 typedef std::list<int>   IntList;
 
+bool CheckArgsIsError(int argc, char *argv[]) {
+	if (argc < 2) {
+		std::cout << "Error" << std::endl;
+		return true;
+	}
+	for (int i = 1; i < argc; i++) {
+		if (std::atoi(argv[i]) <= 0 || !std::strcmp(argv[i], "")) {
+			std::cout << "Error" << std::endl;
+			return true;
+		}
+		for (int j = 0; argv[i][j]; j++) {
+			if (!std::isdigit(argv[i][j])) {
+				std::cout << "Error" << std::endl;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 bool IsSorted(const IntVector &vector, const IntList &list) {
 	if (vector.size() != list.size()) {
 		return false;
@@ -35,25 +55,20 @@ IntList ConvertToIntList(char *argv[], int argc) {
 	return list;
 }
 
-void PrintResult(int argc, char *argv[]) {
+void PrintBefore(int argc, char *argv[]) {
 	std::cout << "Before: ";
 	for (int i = 1; i < argc; i++) {
 		std::cout << argv[i] << " ";
 	}
 	std::cout << std::endl;
-	IntVector vector = ConvertToIntVector(argv, argc);
-	IntList   list   = ConvertToIntList(argv, argc);
+}
 
-	clock_t         start_vec     = clock();
-	const IntVector sorted_vector = PmergeMe::MergeInsertionSortVector(vector);
-	clock_t         end_vec       = clock();
-	double duration_vec = static_cast<double>(end_vec - start_vec) / CLOCKS_PER_SEC * 1000000;
-
-	clock_t       start_list  = clock();
-	const IntList sorted_list = PmergeMe::MergeInsertionSortList(list);
-	clock_t       end_list    = clock();
-	double duration_list = static_cast<double>(end_list - start_list) / CLOCKS_PER_SEC * 1000000;
-
+void PrintResult(
+	const IntVector &sorted_vector,
+	const IntList   &sorted_list,
+	double           duration_vec,
+	double           duration_list
+) {
 	if (IsSorted(sorted_vector, sorted_list)) {
 		std::cout << "After:  ";
 		for (IntVector::const_iterator it = sorted_vector.begin(); it != sorted_vector.end();
@@ -71,27 +86,32 @@ void PrintResult(int argc, char *argv[]) {
 	}
 }
 
+void SortAndPrintResult(int argc, char *argv[]) {
+	PrintBefore(argc, argv);
+
+	IntVector vector = ConvertToIntVector(argv, argc);
+	IntList   list   = ConvertToIntList(argv, argc);
+
+	clock_t         start_vec     = clock();
+	const IntVector sorted_vector = PmergeMe::MergeInsertionSortVector(vector);
+	clock_t         end_vec       = clock();
+	double duration_vec = static_cast<double>(end_vec - start_vec) / CLOCKS_PER_SEC * 1000000;
+
+	clock_t       start_list  = clock();
+	const IntList sorted_list = PmergeMe::MergeInsertionSortList(list);
+	clock_t       end_list    = clock();
+	double duration_list = static_cast<double>(end_list - start_list) / CLOCKS_PER_SEC * 1000000;
+
+	PrintResult(sorted_vector, sorted_list, duration_vec, duration_list);
+}
+
 } // namespace
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		std::cout << "Error" << std::endl;
-		return 1;
-	}
-	for (int i = 1; i < argc; i++) {
-		if (std::atoi(argv[i]) <= 0 || !std::strcmp(argv[i], "")) {
-			std::cout << "Error" << std::endl;
-			return 1;
-		}
-		for (int j = 0; argv[i][j]; j++) {
-			if (!std::isdigit(argv[i][j])) {
-				std::cout << "Error" << std::endl;
-				return 1;
-			}
-		}
+	if (CheckArgsIsError(argc, argv)) {
+		return EXIT_FAILURE;
 	}
 
-	PrintResult(argc, argv);
-
-	return 0;
+	SortAndPrintResult(argc, argv);
+	return EXIT_SUCCESS;
 }
